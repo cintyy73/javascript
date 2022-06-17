80c1b68cdc02eb3156aa3fd58720f3b05af4e2eb
cintyy <cintiapaezdm@gmail.com>
2022-06-16 22:18:14 -0300

    variables, const alert console

---
+++ app.js
@@ -0,0 +1,8 @@
+const edad =25;
+//console .log (edad)
+//alert('Hola ' + 'bienvenida')
+const nombre =prompt ('ingresa aqui tu nombre')
+console.error('usuario no disponible')
+console.warn('alto ahi hacker')
+console.info('texto normal')
+alert('Hola '  + nombre + ' querido')
